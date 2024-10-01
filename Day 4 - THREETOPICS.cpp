282cd00eeed74df73c2c460d79a3b6dc5ad3485b
Vansh Pratap Singh Bhadoriya <145289450+Vanshu0718@users.noreply.github.com>
2024-10-01 23:34:30 +0530

    Add files via upload

---
+++ Day 4 - THREETOPICS.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int A , B , C , X;
+	cin>>A>>B>>C>>X;
+	
+	if(X==A || X==B || X==C){
+	    cout<<"YES";
+	}
+	
+	else{
+	    cout<<"NO";
+	}
+	
+	return 0;
+}
