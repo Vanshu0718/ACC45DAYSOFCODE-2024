24596f4c751416a26619eb33bc89c0e7b5e1523b
Vansh Pratap Singh Bhadoriya <145289450+Vanshu0718@users.noreply.github.com>
2024-10-09 23:28:51 +0530

    Add files via upload

---
+++ Day 12 - FLIPCARDS.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int T; 
+    cin >> T;
+
+    while (T--) {
+        int N, X; 
+        cin >> N >> X;
+
+        int face_down = N - X;
+
+        cout << min(X, face_down) << endl;
+    }
+
+    return 0;
+}
+
