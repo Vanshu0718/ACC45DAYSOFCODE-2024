cbbffeb4f8e76bbf64258311bb1f71eff0fbe3c4
Vansh Pratap Singh Bhadoriya <145289450+Vanshu0718@users.noreply.github.com>
2024-11-02 22:00:44 +0530

    Add files via upload

---
+++ Day 31 - FIZZBUZZ2303.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int N;
+	    cin>>N;
+	    cout<<(N*N) - N<<endl;
+	}
+	return 0;
+
+}
